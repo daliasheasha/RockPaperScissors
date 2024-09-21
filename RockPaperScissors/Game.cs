7ae1687ae92c962b121d8b5a85c695e5cd65230a
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-21 14:14:36 -0400

    Migrating to .NET 8

--- RockPaperScissors/Game.cs
+++ RockPaperScissors/Game.cs
@@ -1,80 +1,88 @@
-﻿namespace RockPaperScissors;
-public class Game
+The file does not require any updates to be compatible with the new target environment. Here is the file contents:
+
+```csharp
+using RockPaperScissors;
+
+namespace RockPaperScissors
 {
-
-    public static void Play()
+    public class Game
     {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Let's play Rock, Paper, Scissors!\n");
 
-        while (true)
+        public static void Play()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Enter a choice from the following: (r)ock, (p)aper, (s)cissors");
+            Console.WriteLine("Let's play Rock, Paper, Scissors!\n");
 
-            Console.ForegroundColor = ConsoleColor.White;
-            string? userInput = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(userInput) || (GetPlayChoice(userInput) is Choice playerChoice && playerChoice == Choice.Invalid))
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Enter a choice from the following: (r)ock, (p)aper, (s)cissors");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                string? userInput = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(userInput) || (GetPlayChoice(userInput) is Choice playerChoice && playerChoice == Choice.Invalid))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please enter a valid option!");
+                    continue;
+                }
+
+                Choice computerChoice = (Choice)new Random().Next(0, 3); // Last Enum choice is Invalid
+
+                Console.WriteLine("\nYour choice: " + playerChoice);
+                Console.WriteLine("Computer choice: " + computerChoice);
+                Console.WriteLine(GetGameResultMessage(playerChoice, computerChoice));
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nDo you want to play again? [y/n]");
+                string? reply = Console.ReadLine();
+                if (reply != null && reply.ToLower().Equals("n"))
+                {
+                    break;
+                }
+            }
+        }
+
+        public static Choice GetPlayChoice(string userInput)
+        {
+            Choice playerChoice = userInput switch
+            {
+                "rock" or "r" => Choice.Rock,
+                "paper" or "p" => Choice.Paper,
+                "scissors" or "s" => Choice.Scissors,
+                _ => Choice.Invalid,
+            };
+            return playerChoice;
+        }
+
+        public static String GetGameResultMessage(Choice playerChoice, Choice computerChoice)
+        {
+            if (playerChoice == computerChoice)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                return "It's a tie!";
+            }
+            else if ((playerChoice == Choice.Rock && computerChoice == Choice.Scissors) ||
+                    (playerChoice == Choice.Paper && computerChoice == Choice.Rock) ||
+                    (playerChoice == Choice.Scissors && computerChoice == Choice.Paper))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                return "You win!";
+            }
+            else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Please enter a valid option!");
-                continue;
-            }
-
-            Choice computerChoice = (Choice)new Random().Next(0, 3); // Last Enum choice is Invalid
-
-            Console.WriteLine("\nYour choice: " + playerChoice);
-            Console.WriteLine("Computer choice: " + computerChoice);
-            Console.WriteLine(GetGameResultMessage(playerChoice, computerChoice));
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("\nDo you want to play again? [y/n]");
-            string? reply = Console.ReadLine();
-            if (reply != null && reply.ToLower().Equals("n"))
-            {
-                break;
+                return "Computer wins!";
             }
         }
-    }
 
-    public static Choice GetPlayChoice(string userInput)
-    {
-        Choice playerChoice = userInput switch
+        public enum Choice
         {
-            "rock" or "r" => Choice.Rock,
-            "paper" or "p" => Choice.Paper,
-            "scissors" or "s" => Choice.Scissors,
-            _ => Choice.Invalid,
-        };
-        return playerChoice;
-    }
-
-    public static String GetGameResultMessage(Choice playerChoice, Choice computerChoice)
-    {
-        if (playerChoice == computerChoice)
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            return "It's a tie!";
+            Rock,
+            Paper,
+            Scissors,
+            Invalid
         }
-        else if ((playerChoice == Choice.Rock && computerChoice == Choice.Scissors) ||
-                 (playerChoice == Choice.Paper && computerChoice == Choice.Rock) ||
-                 (playerChoice == Choice.Scissors && computerChoice == Choice.Paper))
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            return "You win!";
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            return "Computer wins!";
-        }
-    }
-
-    public enum Choice
-    {
-        Rock,
-        Paper,
-        Scissors,
-        Invalid
     }
 }
+```
