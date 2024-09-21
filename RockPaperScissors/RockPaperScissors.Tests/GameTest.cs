c63422b92fcca3be7f84d24bbef22814798420ab
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-21 14:59:15 -0400

    Migrating to .NET 8

---
+++ RockPaperScissors/RockPaperScissors.Tests/GameTest.cs
@@ -0,0 +1,21 @@
+using RockPaperScissors.Game;
+
+namespace RockPaperScissors.Tests;
+
+public class GameTest
+{
+    [Fact]
+    public void TestSettingPlayerChoice()
+    {
+        Assert.Equal(Choice.Rock, GetPlayChoice("r"));
+        Assert.Equal(Choice.Rock, GetPlayChoice("rock"));
+
+        Assert.Equal(Choice.Scissors, GetPlayChoice("scissors"));
+        Assert.Equal(Choice.Scissors, GetPlayChoice("s"));
+
+        Assert.Equal(Choice.Paper, GetPlayChoice("paper"));
+        Assert.Equal(Choice.Paper, GetPlayChoice("p"));
+
+        Assert.Equal(Choice.Invalid, GetPlayChoice("blahh"));
+    }
+}
