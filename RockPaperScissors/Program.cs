41d1fcb82422ada43c1593e3c1a706790ee658be
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-21 14:23:05 -0400

    Migrating to .NET 8

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,8 +1,8 @@
-﻿using RockPaperScissors;
+[FILE START] ﻿using RockPaperScissors;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Game.Play();
+        Game.Start();
     }
-}
+} [FILE END]
